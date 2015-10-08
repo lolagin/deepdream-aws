fe900f22872b40627d528a2dca175072efd0d924
lolagin <jeffrey.rosenthal@gmail.com>
2015-10-08 13:34:30 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 # Disable camera, we don't have any:
 sudo ln /dev/null /dev/raw1394
 
-git clone git@github.com:danielvarga/deepdream-aws.git
+git clone git@github.com:lolagin/deepdream-aws.git
 cd deepdream-aws
 ln -s ~/caffe .
 # Get the googlenet model file:
@@ -11,8 +11,9 @@ wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
 mv bvlc_googlenet.caffemodel caffe/models/bvlc_googlenet/
 
 # Get some input image:
-wget --output-document=daniel.png https://www.gravatar.com/avatar/17d651b2d4092b7810ae23d8d94b8cbd?s=640
-convert daniel.png daniel.jpg
+
+wget --output-document=lols.png http://i.imgur.com/p7sKba6.jpg
+convert lols.png lols.jpg
 
 # Run:
-nohup bash layers.sh daniel.jpg &
+# nohup bash layers.sh lols.jpg &
